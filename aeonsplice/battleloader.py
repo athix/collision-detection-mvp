bdaa393a6177cc38af0b5299e39c8ae1972d9b68
Josh Buker <apate+git@nyx.chat>
2019-01-16 17:59:59 -0800

    Add janky PoC scene renderer

---
+++ aeonsplice/battleloader.py
@@ -0,0 +1,46 @@
+import yaml
+
+class Ship:
+    def __init__(self, options):
+        self.id = options['id']
+        self.model_file = self.getModelFile(options['model'])
+
+    def getModelFile(self, model):
+        known_models = {
+            'cube': "models/frowney",
+            'sphere': "models/smiley"
+        }
+        return known_models.get(model, "models/panda")
+
+    def attach(self, app):
+        self.model = app.loader.loadModel(self.model_file)
+        self.model.reparentTo(app.render)
+        self.model.setScale(0.25, 0.25, 0.25)
+
+    def setPosition(self, position):
+        x = position['x']
+        y = position['y']
+        z = position['z']
+        self.model.setPos(x, y, z)
+
+class BattleLoader:
+    def __init__(self, file, app):
+        with open(file, 'r') as stream:
+            try:
+                self.data = yaml.safe_load(stream)
+            except yaml.YAMLError as e:
+                print(e)
+
+        self.app = app
+        self.ships = {}
+        for ship in self.data['battle']['ships']:
+            temp = Ship(ship)
+            temp.attach(app)
+            self.ships[temp.id] = temp
+
+    def render_scene(self, turn):
+        current_turn = self.data['battle']['turns'][turn]
+        if turn != current_turn['time']:
+            print("Turn does not match time!")
+        for ship in current_turn['ships']:
+            self.ships[ship['ship']].setPosition(ship['position'])
---
+++ aeonsplice/client.py
@@ -0,0 +1,41 @@
+from math import pi, sin, cos
+
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from panda3d.core import Point3
+from battleloader import BattleLoader
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Disable camera controls.
+        self.disableMouse()
+
+        self.battle = BattleLoader('head_on.yml', self)
+        # battle.render_scene(1)
+        self.derp = 1
+        self.derpCounter = 0
+
+        # Add the spinCameraTask procedure to the task manager.
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+    # Define a procedure to mvoe the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0 ,0)
+        if self.derpCounter >= 60:
+            if self.derp == 0:
+                self.derp = 1
+            else:
+                self.derp = 0
+            self.battle.render_scene(self.derp)
+            self.derpCounter = 0
+        else:
+            self.derpCounter += 1
+        return Task.cont
+
+app = MyApp()
+app.run()
