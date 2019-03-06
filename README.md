05a036d6d2c521566942b08080127950f5206974
Joshua Buker <buker.joshua@gmail.com>
2019-03-06 11:21:22 -0800

    Add usage to README.md and clean up client.py

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ Minimal codebase to visualize and simulate continuous collision detection.
 * PyYAML - `pip3 install pyyaml`
 * Panda3D - `pip3 install panda3d`
 
+## Usage
+
+To run the client:
+
+1) `cd ./aeonsplice/`
+2) `python3 client.py`
+
+The client currently accepts `.yaml` files with appropriate formatting. There is
+currently little-to-no input validation, as this is just a quick and dirty
+proof-of-concept.
+
 ## TODO
 
 - [X] ~Pick a visualizer & language~ **(Python w/ Panda3D)**
--- aeonsplice/client.py
+++ aeonsplice/client.py
@@ -16,43 +16,20 @@ class MyApp(ShowBase):
         # Disable camera controls.
         self.disableMouse()
 
+        # TODO: Support manual camera control (WASD&CTRL/SPACE + Mouse)
+
         self.gameFSM = GameFSM(self)
         self.gameFSM.request('MainMenu')
-
-        # self.battle = BattleLoader('head_on.yml', self)
-        # battle.render_scene(1)
-        # self.derp = 1
-        # self.derpCounter = 0
-        # self.battle.render_scene(self.derp)
 
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
-    # def setText(self, textEntered):
-    #     self.textObject.setText(textEntered)
-    #     if hasattr(self, 'battle'):
-    #         self.battle.reset_ships()
-    #     try:
-    #         self.battle = Battle(textEntered, self)
-    #         self.battle.render_turn(0)
-    #     except FileNotFoundError:
-    #         print('Failed to load file: ', textEntered)
-
-    # Define a procedure to mvoe the camera.
+    # Define a procedure to move the camera.
     def spinCameraTask(self, task):
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0 ,0)
-        # if self.derpCounter >= 60:
-        #     if self.derp == 0:
-        #         self.derp = 1
-        #     else:
-        #         self.derp = 0
-        #     self.battle.render_scene(self.derp)
-        #     self.derpCounter = 0
-        # else:
-        #     self.derpCounter += 1
         return Task.cont
 
 app = MyApp()
